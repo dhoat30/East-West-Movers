19af94d2bb07d3291b4d2ec24ac79656f878c45c
dhoat30 <dhoat30@gmail.com>
2025-05-16 21:07:18 +1200

    first commit

---
+++ components/UI/Footer/FooterContact.jsx
@@ -0,0 +1,20 @@
+export const contactInfo = { 
+    info:[ 
+        {
+            url: "tel:1300 556 506", 
+            label: "1300 556 506",
+            icon: { 
+                url: "/phone.png", 
+                alt: "phone icon" 
+            },
+        },
+        {
+            url: "mailto:eastwestmoves@gmail.com", 
+            label: "eastwestmoves@gmail.com", 
+            icon: { 
+                url: "/email.png", 
+                alt: "Email icon" 
+            },
+        }
+    ]
+}
